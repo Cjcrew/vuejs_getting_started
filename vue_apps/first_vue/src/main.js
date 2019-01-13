8173790c45a0857ade12293c4105c55888acaa3f
Cjcrew <cj_crew@aol.com>
2019-01-13 22:04:03 +0200

    Integrate Vue router

--- vue_apps/first_vue/src/main.js
+++ vue_apps/first_vue/src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
 import App from './App'
+import VueRouter from "vue-router"
+import Posts from './components/Posts'
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/', component: Posts
+  }
+]
+
+const router = new VueRouter({routes: routes})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
