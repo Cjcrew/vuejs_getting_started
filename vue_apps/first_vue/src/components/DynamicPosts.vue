56d233d5ebb3db28271a491bbbf58fcc3baf885e
Cjcrew <cj_crew@aol.com>
2019-01-13 21:40:04 +0200

    Create new component to grab data from an API with axios

---
+++ vue_apps/first_vue/src/components/DynamicPosts.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <table class="table mt-4">
+      <thead>
+        <tr>
+          <th scope="col">User Id</th>
+          <th scope="col">Id</th>
+          <th scope="col">Body</th>
+          <th scope="col">Body</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="post in posts" :key="post.id">
+          <td>{{post.id}}</td>
+          <td>{{post.title}}</td>
+          <td>{{post.body}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'DynamicPosts',
+  data() {
+    return {
+      posts: [
+      ]
+    },
+    created() {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+
